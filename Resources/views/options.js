553e822a68774a288d58f7762548e242e789e11b
Krawaller <jacob@krawaller.se>
2010-10-30 16:50:25 +0200

    He makes all things new

--- Resources/views/options.js
+++
@@ -1,9 +0,0 @@
-Ti.include('../js/go.view.pre.js');
-Go.winTitle('options');
-
-currentWin.add(Go.table([{
-    titleKey: 'pickLanguage',
-    url: 'lang.js'
-}]));
-
-Ti.include('../js/go.view.post.js');
