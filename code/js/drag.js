413dca4f74124a4f5dbb9e30a1d6e004b9e89020
Krawaller <jacob@krawaller.se>
2010-10-31 14:29:15 +0100

    Working on multitouch draggables

--- code/js/drag.js
+++ code/js/drag.js
@@ -7,20 +7,44 @@ var body = document.body;
 var touches = {};
 	
 document.body.addEventListener(touchstart, function(e){
-	var t = e.changedTouches ? e.changedTouches[0] : e;
-	//var el = e.target && (e.target.nodeType == 3 ? e.target.parentNode : e.target);
-	var el = e.target;
-	while(!el.draggable && el != body && el = el.parentNode);
+	e.preventDefault();
+	var ts = e.changedTouches ? e.changedTouches : [e], t, i = ts.length, el;
 	
-	console.log(el);
+	while(t = ts[--i]){
+		el = t.target;
+		while(el.className.indexOf('draggable') == -1 && (el = (el != body)) && (el = el.parentNode));
+		if(el){
+			touches[t.identifier] = { el: el, hover: null };
+			$(el).addClass('dragging');
+			//console.log('touchme', el)
+		}
+	}
 }, false);
 
 document.body.addEventListener(touchmove, function(e){
-	var t = e.changedTouches ? e.changedTouches[0] : e;
+	e.preventDefault();
+	var ts = e.changedTouches ? e.changedTouches : [e], i = ts.length, el, o;
+	while((t = ts[--i]) && (o = touches[t.identifier])){
+		//console.log(t, el);
+		el = o.el;
+		el.style.webkitTransform = 'translate3d(' + t.pageX + 'px, ' + t.pageY + 'px, 0px)';
+		var hover = document.elementFromPoint(t.pageX, t.pageY);
+		if(o.hover != hover){
+			$(o.hover).removeClass('hover');
+			$(hover).addClass('hover');
+			o.hover = hover;
+		}
+	}
 }, false);
 
-document.body.addEventListener(touchend, function(){
-	var t = e.changedTouches ? e.changedTouches[0] : e;
-}
+document.body.addEventListener(touchend, function(e){
+	e.preventDefault();
+	var ts = e.changedTouches ? e.changedTouches : [e], i = ts.length, el;
+	while((t = ts[--i]) && (o = touches[t.identifier])){
+	el = o.el;
+		$(el).removeClass('dragging');
+		delete touches[i];
+	}
+});
 
 })();
